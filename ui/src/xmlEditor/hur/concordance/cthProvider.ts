aae2b99224835a16731bb43529b4f48f9fc06ba0
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-10 17:41:46 +0300

    Add further error handling

--- ui/src/xmlEditor/hur/concordance/cthProvider.ts
+++ ui/src/xmlEditor/hur/concordance/cthProvider.ts
@@ -3,8 +3,25 @@ import { updateMapping } from '../common/utility';
 const textToCTHGroup = new Map<string, string>();
 
 fetch('textToCTHGroup.json')
-  .then(response => response.json())
-  .then(object => updateMapping(textToCTHGroup, object));
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Unexpected response status ${response.status}`);
+    } else {
+      const contentType = response.headers.get('Content-Type');
+      if (contentType !== null && contentType.startsWith('application/json')) {
+        return response.json();
+      } else {
+        //response.text().then(text => console.log(text));
+        throw new Error(`Unexpected content type ${contentType}`);
+      }
+    }
+  })
+  .then(obj => {
+    updateMapping(textToCTHGroup, obj);
+  })
+  .catch(error => {
+    console.log('Error while fethcing the text to CTH group mapping:', error);
+  });
 
 const unknown = '(unknown)';
 
