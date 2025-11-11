5293752f1afb3dc1a606327d40830bdd53bbe738
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-11 22:37:57 +0300

    Fix CTH lookup

--- ui/src/xmlEditor/hur/concordance/cthProvider.ts
+++ ui/src/xmlEditor/hur/concordance/cthProvider.ts
@@ -1,32 +1,9 @@
-import { updateMapping } from '../common/utility';
-
-const textToCTHGroup = new Map<string, string>();
-
-fetch('/textToCTHGroup.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Unexpected response status ${response.status}`);
-    } else {
-      const contentType = response.headers.get('Content-Type');
-      if (contentType !== null && contentType.startsWith('application/json')) {
-        return response.json();
-      } else {
-        //response.text().then(text => console.log(text));
-        throw new Error(`Unexpected content type ${contentType}`);
-      }
-    }
-  })
-  .then(obj => {
-    updateMapping(textToCTHGroup, obj);
-  })
-  .catch(error => {
-    console.log('Error while fethcing the text to CTH group mapping:', error);
-  });
+import { textToCthGroup } from './textToCthGroup';
 
 const unknown = '(unknown)';
 
 export function getCTH(textName: string): string {
-  const cth = textToCTHGroup.get(textName);
+  const cth = textToCthGroup.get(textName);
   if (cth === undefined) {
     return unknown;
   } else {
