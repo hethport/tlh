2342f032e24d2d7cddcab4b3e3c6b4b6289d2291
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-12 01:28:18 +0300

    Add a test for morpheme label replacement

---
+++ ui/src/xmlEditor/hur/dictionaryViewer/replaceMorphemeLabel.spec.ts
@@ -0,0 +1,22 @@
+import { replaceMorphemeLabel } from './morphemics';
+
+const modification = replaceMorphemeLabel('-EQU', '-EQU2', '-nna');
+
+describe('modifying morphological analyses', () => {
+
+  test.each<[[string, string], string]>([
+    [['āri-ne-nna', 'RELAT.SG-EQU'], 'RELAT.SG-EQU2'],
+    [['nurandiġi-nna', 'EQU'], 'EQU2'],
+    [['ḫinzori-nna', 'EQU'], 'EQU2'],
+    [['naḫḫ+o+bade-oš=nna', 'EQU=3SG.ABS'], 'EQU=3SG.ABS'],
+    [['Teššob-pe-ož=l', 'GEN-EQU=3PL.ABS'], 'GEN-EQU=3PL.ABS'],
+    [['kiv+ōr+ġē-ne-v-ōž', 'RELAT.SG-GEN-EQU'], 'RELAT.SG-GEN-EQU'],
+    [['kāze-ōž', 'Becher-EQU'], 'Becher-EQU']
+  ])(
+    'for %j, the morphological tag should be %s',
+    ([segmentation, morphTag], newMorphTag) =>
+    expect(modification(segmentation, morphTag))
+    .toEqual(newMorphTag)
+  );
+
+});
