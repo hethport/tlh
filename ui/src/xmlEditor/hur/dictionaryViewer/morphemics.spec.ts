8d750210132fcfcf0be866fd4b959a865ff1e05d
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-11 19:32:29 +0300

    Simplify the test data

--- ui/src/xmlEditor/hur/dictionaryViewer/morphemics.spec.ts
+++ ui/src/xmlEditor/hur/dictionaryViewer/morphemics.spec.ts
@@ -1,32 +1,34 @@
 import { getInflectionalSuffixesAndEnclitics } from './morphemics';
-import { GrammaticalMorpheme as GM } from './grammaticalMorpheme';
+import { GrammaticalMorpheme } from './grammaticalMorpheme';
 
 describe('aligning a string of morphemes and a morphological tag', () => {
 
-  test.each<[[string, string], GM[]]>([
+  test.each<[[string, string], [string, string][]]>([
     [
       ['MED-PL.A-3A-NMLZ-RELAT.PL.ABS=CON', '-u-nd-a-šše-na=mma'],
       [
-        new GM('-MED', '-u'),
-        new GM('-PL.A', '-nd'),
-        new GM('-3A', '-a'),
-        new GM('-NMLZ', '-šše'),
-        new GM('-RELAT.PL.ABS', '-na'),
-        new GM('=CON', '=mma')
+        ['-MED', '-u'],
+        ['-PL.A', '-nd'],
+        ['-3A', '-a'],
+        ['-NMLZ', '-šše'],
+        ['-RELAT.PL.ABS', '-na'],
+        ['=CON', '=mma']
       ]
     ],
     [
       ['.ABS=CON', '=an'],
       [
-        new GM('.ABS', ''),
-        new GM('=CON', '=an')
+        ['.ABS', ''],
+        ['=CON', '=an']
       ]
     ],
   ])(
     'for %j, an array of grammatical morphemes %j should be returned',
-     ([morphTag, gramString], grammaticalMorphemes) =>
+     ([morphTag, gramString], labelFormPairs) =>
      expect(getInflectionalSuffixesAndEnclitics(morphTag, gramString))
-      .toEqual(grammaticalMorphemes)
+      .toEqual(labelFormPairs.map(([label, form]) =>
+        new GrammaticalMorpheme(label, form))
+      )
   );
 
 });
