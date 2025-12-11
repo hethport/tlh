d739821d54f0e6e0677290bf2392b9bfb84be451
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-11 10:09:28 +0300

    Add tests for morpheme - gloss alignment

---
+++ ui/src/xmlEditor/hur/dictionaryViewer/morphemics.spec.ts
@@ -0,0 +1,32 @@
+import { getInflectionalSuffixesAndEnclitics } from './morphemics';
+import { GrammaticalMorpheme as GM } from './grammaticalMorpheme';
+
+describe('aligning a string of morphemes and a morphological tag', () => {
+
+  test.each<[[string, string], GM[]]>([
+    [
+      ['-u-nd-a-šše-na=mma', 'MED-PL.A-3A-NMLZ-RELAT.PL.ABS=CON'],
+      [
+        new GM('-MED', '-u'),
+        new GM('-PL.A', '-nd'),
+        new GM('-3A', '-a'),
+        new GM('-NMLZ', '-šše'),
+        new GM('-RELAT.PL.ABS', '-na'),
+        new GM('=CON', '=mma')
+      ]
+    ],
+    [
+      ['=an', '.ABS=CON'],
+      [
+        new GM('.ABS', ''),
+        new GM('=CON', '=an')
+      ]
+    ],
+  ])(
+    'for %j, an array of grammatical morphemes %j should be returned',
+     ([gramString, morphTag], grammaticalMorphemes) =>
+     expect(getInflectionalSuffixesAndEnclitics(gramString, morphTag))
+      .toEqual(grammaticalMorphemes)
+  );
+
+});
