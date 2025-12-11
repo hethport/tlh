4af3c676490b35a65d6ee90f52d2a47e8a3b9326
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-11 19:55:49 +0300

    Add more tests for morpheme-gloss alignment (not all passed at the moment)

--- ui/src/xmlEditor/hur/dictionaryViewer/morphemics.spec.ts
+++ ui/src/xmlEditor/hur/dictionaryViewer/morphemics.spec.ts
@@ -22,6 +22,81 @@ describe('aligning a string of morphemes and a morphological tag', () => {
         ['=CON', '=an']
       ]
     ],
+    [
+      ['RELAT.PL.ABS=CON', '-na=m'],
+      [
+        ['-RELAT.PL', '-na'],
+        ['.ABS', ''],
+        ['=CON', '=m']
+      ]
+    ],
+    [
+      ['3POSS-PL.ABS=3PL.ABS', '-ī-až=l'],
+      [
+        ['-3POSS', '-ī'],
+        ['-PL', '-až'],
+        ['.ABS', ''],
+        ['=3PL.ABS', '=l']
+      ]
+    ],
+    [
+      ['PL.A-PAT-PTCP.ABS', '-id-ō-re'],
+      [
+        ['-PL.A', '-id'],
+        ['-PAT', '-ō'],
+        ['-PTCP', '-re'],
+        ['.ABS', '']
+      ]
+    ],
+    [
+      ['1A.SG-NOM.ABS', '-aw-šše'],
+      [
+        ['-1A.SG', '-aw'],
+        ['-NOM', '-šše'],
+        ['.ABS', '']
+      ]
+    ],
+    [
+      ['1POSS.SG.ABS=3PL.ABS', '-iffe=l'],
+      [
+        ['-1POSS.SG', '-iffe'],
+        ['.ABS', ''],
+        ['=3PL.ABS', '=l']
+      ]
+    ],
+    [
+      ['RELAT.PL-PL-DAT/ESS=3PL.ABS', '-na-až-a=lla'],
+      [
+        ['-RELAT.PL', '-na'],
+        ['-PL', '-až'],
+        ['-DAT/ESS', '-a'],
+        ['=3PL.ABS', '=lla']
+      ]
+    ],
+    [
+      ['MOD.ACT-POT=1SG.ABS', '-i-evā=tta'],
+      [
+        ['-MOD.ACT', '-i'],
+        ['-POT', '-evā'],
+        ['=1SG.ABS', '=tta']
+      ]
+    ],
+    [
+      ['.ABS=2PL.ABS=CON', '=ffa=an'],
+      [
+        ['.ABS', ''],
+        ['=2PL.ABS', '=ffa'],
+        ['=CON', '=an']
+      ]
+    ],
+    [
+      ['uġ-3A.SG=2PL.ABS', '-uġ-a=ffa'],
+      [
+        ['-uġ', '-uġ'],
+        ['-3A.SG', '-a'],
+        ['=2PL.ABS', '=ffa']
+      ]
+    ]
   ])(
     'for %j, an array of grammatical morphemes %j should be returned',
      ([morphTag, gramString], labelFormPairs) =>
