4d8a2286ca3109e303272982b3f014d41aa0a3c1
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-30 10:59:30 +0300

    Disable applying changes with the Macroeditor

--- ui/src/xmlEditor/hur/macroeditor/Macroeditor.tsx
+++ ui/src/xmlEditor/hur/macroeditor/Macroeditor.tsx
@@ -1,17 +1,24 @@
 import { JSX, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { ChangesViewerContainer } from '../changes/ChangesViewerContainer';
-import { TextEditorContainer } from '../textEditor/TextEditorContainer';
+import { ChangesListClearer } from '../textEditor/ChangesListClearer';
 
 interface IProps {
   initialChanges: Map<string, string>;
 }
 
 export function Macroeditor({ initialChanges }: IProps): JSX.Element {
+  const {t} = useTranslation('common');
   const [changes, setChanges] = useState(initialChanges);
+  const clearChanges = () => setChanges(new Map<string, string>());
   return (
     <div className="grid grid-cols-2 gap-2 my-2">
       <ChangesViewerContainer changes={changes} />
-      <TextEditorContainer clearChanges={() => setChanges(new Map<string, string>())} />
+      <div className="container mx-auto">
+        <h2 className="font-bold text-2xl text-center">{t('textEditor')}</h2>
+        <br/>
+        <ChangesListClearer clearChanges={clearChanges} />
+      </div>
     </div>
   );
 }
