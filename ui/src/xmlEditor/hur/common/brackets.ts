64f31693f682ed9320654536f9504da5d2d07b45
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-17 17:19:58 +0100

    Repeat unsuccessful dictionary lookup with removed brackets only if the transcription really contains brackets

--- ui/src/xmlEditor/hur/common/brackets.ts
+++ ui/src/xmlEditor/hur/common/brackets.ts
@@ -1,4 +1,9 @@
 const brackets = /[[\]]/g;
+
+export function containsBrackets(transcription: string): boolean {
+  return transcription.search(brackets) !== -1;
+}
+
 export function removeBrackets(transcription: string): string {
   return transcription.replaceAll(brackets, '');
 }
