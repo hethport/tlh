384fb27f0e7dfbb74d8dd25aec51466faa303f8c
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-18 14:56:24 +0100

    Remove brackets from the transcription when updating the Hurrian dictionary (unless the segmentation contains brackets)

--- ui/src/xmlEditor/hur/dict/dictionaryUpdater.ts
+++ ui/src/xmlEditor/hur/dict/dictionaryUpdater.ts
@@ -1,11 +1,26 @@
 import { basicUpdateHurrianDictionary } from '../dict/dictionary';
 import { readMorphAnalysisValue } from '../morphologicalAnalysis/auxiliary';
 import { basicSaveGloss } from '../translations/glossUpdater';
+import { containsBrackets, removeBrackets } from '../common/brackets';
 
 export function addMorphologicalAnalysis(transcription: string, analysis: string) {
-  basicUpdateHurrianDictionary(transcription, analysis);
   const morphologicalAnalysis = readMorphAnalysisValue(analysis);
   if (morphologicalAnalysis !== undefined) {
+    const segmentation = morphologicalAnalysis.referenceWord;
+    let normalizedTranscription;
+    // Words should be added to the dictionary without brackets
+    // because the same word can occur with brackets
+    // in different positions.
+    // However, if the segmentation contains brackets,
+    // it should only be suggested for words with
+    // identical bracketing, so in this case,
+    // brackets in the transcription should be preserved.
+    if (containsBrackets(transcription) && !containsBrackets(segmentation)) {
+      normalizedTranscription = removeBrackets(transcription);
+    } else {
+      normalizedTranscription = transcription;
+    }
+    basicUpdateHurrianDictionary(normalizedTranscription, analysis);
     basicSaveGloss(morphologicalAnalysis);
   }
 }
