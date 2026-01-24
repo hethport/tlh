40db2d619661c20c90f5dc987cadcbe39f8cad27
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-24 15:42:11 +0100

    Add a new function for the selected check, which can also handle analyses with enclitics

--- ui/src/xmlEditor/nodePostprocessing.ts
+++ ui/src/xmlEditor/nodePostprocessing.ts
@@ -1,8 +1,28 @@
 import { XmlElementNode } from 'simple_xml';
 import { readSelectedMorphology, SelectedMorphAnalysis } from '../model/selectedMorphologicalAnalysis';
 import { readMorphologiesFromNode, MorphologicalAnalysis } from '../model/morphologicalAnalysis';
-import { isSelected } from './hur/morphologicalAnalysis/auxiliary';
 import update, { Spec } from 'immutability-helper';
+
+function isSelected(morphologicalAnalysis: MorphologicalAnalysis): boolean {
+  switch (morphologicalAnalysis._type) {
+    case 'SingleMorphAnalysisWithoutEnclitics':
+      return morphologicalAnalysis.selected;
+    case 'SingleMorphAnalysisWithSingleEnclitics':
+      return morphologicalAnalysis.selected;
+    case 'SingleMorphAnalysisWithMultiEnclitics':
+      return morphologicalAnalysis.encliticsAnalysis.analysisOptions
+        .some(analysisOption => analysisOption.selected);
+    case 'MultiMorphAnalysisWithoutEnclitics':
+      return morphologicalAnalysis.analysisOptions.some(analysisOption => analysisOption.selected);
+    case 'MultiMorphAnalysisWithSingleEnclitics':
+      return morphologicalAnalysis.analysisOptions.some(analysisOption => analysisOption.selected);
+    case 'MultiMorphAnalysisWithMultiEnclitics':
+      return morphologicalAnalysis.selectedAnalysisCombinations.length > 0;
+    default:
+      // This case should never occur because the list of cases abover is exhaustive.
+      return true;
+  }
+}
 
 export function postprocessNode(node: XmlElementNode): XmlElementNode {
   const selectedMorphologies: SelectedMorphAnalysis[] = node.attributes.mrp0sel !== undefined
