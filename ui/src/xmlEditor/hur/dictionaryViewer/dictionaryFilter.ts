0242b881e1769fc3d72a1d9459519f04baaa4eee
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-29 17:22:50 +0100

    Filter out stems which contain one or more 'x' characters in brackets

--- ui/src/xmlEditor/hur/dictionaryViewer/dictionaryFilter.ts
+++ ui/src/xmlEditor/hur/dictionaryViewer/dictionaryFilter.ts
@@ -1,3 +1,9 @@
+const embracketedFragmentarySigns = /\[x+\]/;
+function containsEmbracketedFragmentarySigns(stem: string): boolean {
+  return stem.search(embracketedFragmentarySigns) !== -1;
+}
+
 export function stemIsFragmentary(stem: string) {
-  return stem === '' || stem === '[' || stem.startsWith(']');
+  return stem === '' || stem === '[' || stem.startsWith(']') ||
+    containsEmbracketedFragmentarySigns(stem);
 }
