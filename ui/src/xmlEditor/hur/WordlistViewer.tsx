d636157c39330701a77c5c18e09950da42bff7ae
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-17 15:09:41 +0100

    Add stop list export and import and removing entries from the stop list

--- ui/src/xmlEditor/hur/WordlistViewer.tsx
+++ ui/src/xmlEditor/hur/WordlistViewer.tsx
@@ -4,28 +4,34 @@ import { writeMorphAnalysisValue } from '../../model/morphologicalAnalysis';
 
 interface IProps {
   entries: Entry[];
+  removeEntry: (entry: Entry) => void;
 }
 
 function doNothing(): void {
   // Do nothing
 }
 
-export function WordlistViewer({ entries }: IProps): JSX.Element {
+export function WordlistViewer({ entries, removeEntry }: IProps): JSX.Element {
   return (
-    <div>
+    <div className="mt-2">
     {entries.map((entry: Entry) => {
       const { transcriptions, initialMorphologicalAnalysis } = entry;
       const analysis = writeMorphAnalysisValue(initialMorphologicalAnalysis);
       const key = transcriptions.join(',') + '@' + analysis;
-      return (<WordformElement
-        key={key}
-        entry={entry}
-        handleSegmentationInput={doNothing}
-        handleSegmentationBlur={doNothing}
-        handleAnalysisInput={doNothing}
-        handleAnalysisBlur={doNothing}
-        initialShowAttestations={false}
-      />);
+      return (<div className="flex flex-row" key={key}>
+        <WordformElement
+          entry={entry}
+          handleSegmentationInput={doNothing}
+          handleSegmentationBlur={doNothing}
+          handleAnalysisInput={doNothing}
+          handleAnalysisBlur={doNothing}
+          initialShowAttestations={false}/>
+        <button type="button" className="p-2 rounded-r border border-slate-500"
+          onClick={() => removeEntry(entry)}
+          title={'removeFromTheList'}>
+          &#10754;
+        </button>
+      </div>);
     })}
     </div>
   );
