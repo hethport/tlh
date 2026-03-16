f3be473b1ebd0ae6fdffa997b1cab102a5878970
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-16 15:44:00 +0100

    Add additional lookup options in the settings (theire effects are not yet implemented)

--- ui/src/xmlEditor/lookupConfig.ts
+++ ui/src/xmlEditor/lookupConfig.ts
@@ -1,9 +1,15 @@
 export interface LookupConfig {
   ignorePlene: boolean;
+  mergeLabials: boolean;
+  mergeMidAndHighVowels: boolean;
+  ignoreVoice: boolean;
 }
 
 export const defaultLookupConfig = {
   ignorePlene: false,
+  mergeLabials: false,
+  mergeMidAndHighVowels: false,
+  ignoreVoice: false,
 };
 
 export const lookupConfigKeys = Object.keys(defaultLookupConfig);
