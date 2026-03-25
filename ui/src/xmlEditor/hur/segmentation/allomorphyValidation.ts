97a9177255a0b1b2d6386b5f4364ae52b810f5fa
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-25 15:39:30 +0100

    Allow arbitrary differences between Sumerograms and their Hurrian readings, use a separate function for stem allomorphy validation

--- ui/src/xmlEditor/hur/segmentation/allomorphyValidation.ts
+++ ui/src/xmlEditor/hur/segmentation/allomorphyValidation.ts
@@ -3,6 +3,7 @@
 const finalVowelNotAfterCoronalSonorant = /(?<![lnr])[aeiouāēīōū]$/;
 const finalVowel = /[aeiouāēīōū]$/;
 const initialVowel = /^[-=]?[aeiouāēīōū]/;
+const sumerogram = /^(\p{Lu}|[-.])+$/u;
 
 function endsWithVowelNotAfterCoronalSonorant(stem: string): boolean {
   return finalVowelNotAfterCoronalSonorant.test(stem);
@@ -16,11 +17,25 @@ function startsWithVowel(ending: string): boolean {
   return initialVowel.test(ending);
 }
 
-export function allomorphyIsValid(surfaceStem: string, underlyingStem: string,
-                                  surfaceSuffixChain: string): boolean {
+function isSumerogram(stem: string): boolean {
+  return sumerogram.test(stem);
+}
+
+function stemAllomorphyIsValid(surfaceStem: string, underlyingStem: string,
+                               surfaceSuffixChain: string): boolean {
+  if (isSumerogram(surfaceStem) && !isSumerogram(underlyingStem)) {
+    // Any differences are allowed between Sumerograms
+    // and their Hurrian readings.
+    return true;
+  }
   if (endsWithVowelNotAfterCoronalSonorant(underlyingStem) && !endsWithVowel(surfaceStem)) {
     // The final vowel of the stem was deleted, and it is not after l, n or r.
     return startsWithVowel(surfaceSuffixChain);
   }
   return true;
+}
+
+export function allomorphyIsValid(surfaceStem: string, underlyingStem: string,
+                                  surfaceSuffixChain: string): boolean {
+  return stemAllomorphyIsValid(surfaceStem, underlyingStem, surfaceSuffixChain);
 }
