9a46977a456aa1713b1d92cea49a6f055f7949f8
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-30 11:50:57 +0200

    Do not annotate automatically fragments with only two letters and a closing bracket between them

--- ui/src/xmlEditor/hur/dict/shouldBeAnnotated.ts
+++ ui/src/xmlEditor/hur/dict/shouldBeAnnotated.ts
@@ -1,11 +1,19 @@
 const finalOpeningBracketWithOneOptionalLetter = /(?<!\(-\))\[\p{Ll}?$/u;
+const fragmentWithClosingBracket = /^\p{Ll}\]\p{Ll}$/u;
 
 function endsWithOpeningBracketWithOneOptionalLetter(form: string): boolean {
   return finalOpeningBracketWithOneOptionalLetter.test(form);
 }
 
+function isFragmentWithClosingBracket(form: string): boolean {
+  return fragmentWithClosingBracket.test(form);
+}
+
 export function shouldBeAnnotated(word: string): boolean {
   if (endsWithOpeningBracketWithOneOptionalLetter(word)) {
+    return false;
+  }
+  if (isFragmentWithClosingBracket(word)) {
     return false;
   }
   return true;
