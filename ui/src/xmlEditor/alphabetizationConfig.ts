07026b2b8d30c94c7dc88a6e0d5bd855169882fc
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-31 16:07:40 +0200

    Add two additional alphabetization options (not yet implemented)

--- ui/src/xmlEditor/alphabetizationConfig.ts
+++ ui/src/xmlEditor/alphabetizationConfig.ts
@@ -1,12 +1,16 @@
 export const alphabetizationConfigKey = 'aphabetizationPreferences';
 
 export interface AlphabetizationConfig {
+  alphabetizeFAsP: boolean;
+  alphabetizeVAsB: boolean;
   alphabetizeIAsE: boolean;
   alphabetizeOAsU: boolean;
   alphabetizeVoicedConsonantsAsVoiceless: boolean;
 }
 
 export const defaultAlphabetizationConfig: AlphabetizationConfig = {
+  alphabetizeFAsP: false,
+  alphabetizeVAsB: false,
   alphabetizeIAsE: false,
   alphabetizeOAsU: false,
   alphabetizeVoicedConsonantsAsVoiceless: false,
