457bce3f5b93ed4a510ae103a521fb19c5f6968f
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-04-02 19:19:40 +0200

    Delete not selected options for analyses with enclitics

--- ui/src/xmlEditor/morphologyPostprocessing.ts
+++ ui/src/xmlEditor/morphologyPostprocessing.ts
@@ -1,4 +1,7 @@
-import { MorphologicalAnalysis, readMorphologiesFromNode, writeMorphAnalysisValue }
+import { MorphologicalAnalysis, readMorphologiesFromNode, writeMorphAnalysisValue,
+         MultiMorphologicalAnalysisWithoutEnclitics, MultiMorphologicalAnalysisWithSingleEnclitics,
+         SingleMorphologicalAnalysisWithMultiEnclitics as SingleMorphWithMultiEncl,
+         MultiMorphologicalAnalysisWithMultiEnclitics as MultiMorphWithMultiEncl }
   from '../model/morphologicalAnalysis';
 import { readSelectedMorphology, SelectedMorphAnalysis }
   from '../model/selectedMorphologicalAnalysis';
@@ -6,16 +9,61 @@ import { SelectableLetteredAnalysisOption as Option } from '../model/analysisOpt
 import { XmlElementNode, Attributes } from 'simple_xml';
 import update, { Spec } from 'immutability-helper';
 
-function postprocessMorphology(ma: MorphologicalAnalysis): MorphologicalAnalysis | null {
-  if (ma._type === 'MultiMorphAnalysisWithoutEnclitics') {
-    const { analysisOptions } = ma;
-    const newAnalysisOptions: Option[] = analysisOptions
-      .filter((analysisOption: Option) => analysisOption.selected);
-    if (newAnalysisOptions.length < analysisOptions.length) {
-      return update(ma, { analysisOptions: { $set: newAnalysisOptions } });
-    }
+type MultiMorphWithNoOrSingleEncl = MultiMorphologicalAnalysisWithoutEnclitics |
+                                    MultiMorphologicalAnalysisWithSingleEnclitics;
+
+function deleteNotSelectedAnalysisOptions(ma: MultiMorphWithNoOrSingleEncl): MultiMorphWithNoOrSingleEncl {
+  const { analysisOptions } = ma;
+  const newAnalysisOptions: Option[] = analysisOptions
+    .filter((analysisOption: Option) => analysisOption.selected);
+  if (newAnalysisOptions.length < analysisOptions.length) {
+    return update(ma, { analysisOptions: { $set: newAnalysisOptions } });
+  } else {
+    return ma;
   }
-  return null;
+}
+
+function deleteNotSelectedEncliticsAnalysisOptions(ma: SingleMorphWithMultiEncl): SingleMorphWithMultiEncl {
+  const { analysisOptions } = ma.encliticsAnalysis;
+  const newAnalysisOptions: Option[] = analysisOptions
+    .filter((analysisOption: Option) => analysisOption.selected);
+  if (newAnalysisOptions.length < analysisOptions.length) {
+    return update(ma, { encliticsAnalysis: { analysisOptions: { $set: newAnalysisOptions } } });
+  } else {
+    return ma;
+  }
+}
+
+function deleteNotSelectedOptions(ma: MultiMorphWithMultiEncl): MultiMorphWithMultiEncl {
+  const { analysisOptions, encliticsAnalysis, selectedAnalysisCombinations } = ma;
+  const newAnalysisOptions = analysisOptions.filter(analysisOption =>
+    selectedAnalysisCombinations.some(combination => combination.morphLetter === analysisOption.letter)
+  );
+  const newEncliticsAnalysisOptions = encliticsAnalysis.analysisOptions.filter(analysisOption =>
+    selectedAnalysisCombinations.some(combination => combination.encLetter === analysisOption.letter)
+  );
+  return update(ma, { analysisOptions: { $set: newAnalysisOptions },
+                      encliticsAnalysis: { analysisOptions: { $set: newEncliticsAnalysisOptions } } });
+}
+
+function postprocessMorphology(morphologicalAnalysis: MorphologicalAnalysis): MorphologicalAnalysis {
+  switch (morphologicalAnalysis._type) {
+    case 'SingleMorphAnalysisWithoutEnclitics':
+      return morphologicalAnalysis;
+    case 'SingleMorphAnalysisWithSingleEnclitics':
+      return morphologicalAnalysis;
+    case 'SingleMorphAnalysisWithMultiEnclitics':
+      return deleteNotSelectedEncliticsAnalysisOptions(morphologicalAnalysis);
+    case 'MultiMorphAnalysisWithoutEnclitics':
+      return deleteNotSelectedAnalysisOptions(morphologicalAnalysis);
+    case 'MultiMorphAnalysisWithSingleEnclitics':
+      return deleteNotSelectedAnalysisOptions(morphologicalAnalysis);
+    case 'MultiMorphAnalysisWithMultiEnclitics':
+      return deleteNotSelectedOptions(morphologicalAnalysis);
+    default:
+      // This case should never occur because the list of cases abover is exhaustive.
+      return morphologicalAnalysis;
+  }
 }
 
 function generateSpecWithPostprocessedMorphologies(node: XmlElementNode): Spec<XmlElementNode> | null {
