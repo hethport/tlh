cf11598e69cabb0bfa063cd8e90da990b358bba5
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-07-13 15:03:58 +0200

    Mark yellow Hurrian words which are restored and not annotated

--- ui/src/xmlEditor/hur/colourMarking/isFragmentary.ts
+++ ui/src/xmlEditor/hur/colourMarking/isFragmentary.ts
@@ -1,18 +1,28 @@
 import {XmlElementNode, isXmlElementNode, isXmlTextNode} from 'simple_xml';
 
-const fragmentaryFormMarkerElements = new Set(['del_in', 'del_fin']);
-
 export function isFragmentary(node: XmlElementNode<'w'>): boolean {
+  let bracketOpen = false;
   for (const child of node.children) {
     if (isXmlElementNode(child)) {
-      if (fragmentaryFormMarkerElements.has(child.tagName)) {
-        return true;
+      switch(child.tagName) {
+        case 'del_in':
+          bracketOpen = true;
+          break;
+        case 'del_fin':
+          if (!bracketOpen) {
+            return true;
+          }
+          bracketOpen = false;
+          break;
       }
     } else if (isXmlTextNode(child)) {
       if (child.textContent.includes('x')) {
         return true;
       }
     }
+  }
+  if (bracketOpen) {
+    return true;
   }
   return false;
 }
