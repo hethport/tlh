1d6c9ef6cbd11eb0adca8ed548adb62998d4ee52
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-07-20 11:48:50 +0200

    Allow final vowels in all nouns and adjectives do be deleted

--- ui/src/xmlEditor/hur/segmentation/allomorphGeneration.ts
+++ ui/src/xmlEditor/hur/segmentation/allomorphGeneration.ts
@@ -2,17 +2,17 @@ import { Stem, preprocessStem } from './basicSegmenter';
 import { LookupConfig } from '../../lookupConfig';
 
 const partsOfSpeechElidingFinalVowel = ['noun', 'ADJ'];
-const finalVowelAfterCoronalSonorant = /(?<=[lnr])[aeiouāēīōū]$/;
+const finalVowel = /[aeiouāēīōū]$/;
 
-function endsWithVowelAfterCoronalSonorant(stem: string): boolean {
-  return finalVowelAfterCoronalSonorant.test(stem);
+function endsWithVowel(stem: string): boolean {
+  return finalVowel.test(stem);
 }
 
 export function generateSurfaceAllomorphs(stem: Stem, lookupConfig: LookupConfig, pos: string): string[] {
   const surfaceAllomorphs: string[] = [];
   const mainAllomorph = preprocessStem(stem.form, lookupConfig);
   if (partsOfSpeechElidingFinalVowel.includes(pos)) {
-    if (endsWithVowelAfterCoronalSonorant(mainAllomorph)) {
+    if (endsWithVowel(mainAllomorph)) {
       const surfaceAllomorph = mainAllomorph.substring(0, mainAllomorph.length - 1);
       surfaceAllomorphs.push(surfaceAllomorph);
     }
