cdd82bb18e084e909e28ccd89ad29fa3eb6419a8
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-07-31 10:36:25 +0200

    Remove the marker "???" from mrp0sel before adding the number of a selected analysis

--- ui/src/xmlEditor/elementEditors/selectionPreprocessing.ts
+++ ui/src/xmlEditor/elementEditors/selectionPreprocessing.ts
@@ -1,13 +1,17 @@
 import {removePotentiallyImproperPrefix} from '../hur/common/auxiliary';
 
 const fragmentaryFormSelection = 'DEL';
+const unclearFormSelection = '???';
 
 /**
  * Remove the marking of a form as fragmentary
+ * or unclear
  * before adding the number of a selected
  * morphological analysis.
  */
 export function preprocessSelection(selection: string): string {
   selection = removePotentiallyImproperPrefix(selection, fragmentaryFormSelection);
+  selection = removePotentiallyImproperPrefix(selection,
+                                              unclearFormSelection);
   return selection;
 }
